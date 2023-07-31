dc742e51f0d2ab36e1bddf991a4ef37c87d8a1a8
Yuyang-Su <yuyang.su1@students.mq.edu.au>
2023-07-31 11:28:43 +1000

    lecture work in main

---
+++ src/Person.java
@@ -0,0 +1,35 @@
+public class Person {
+    //The person's name according to the book of names in which all names are recorded
+    final String name;
+    //The person's height in millimeters (as provided by the person)
+    final int heightMm;
+    final boolean likesPizza;
+
+    public Person(String name, int heightMm) {
+        this.name = name;
+        this.heightMm = heightMm;
+        this.likesPizza = false;
+    }
+
+    public Person(int heightMm) {
+        this(name:"(Unknown)", heightMm);
+    }
+
+    public Person() {
+        this(heightMm: 500);
+    }
+
+    @Override
+    public String toString() {
+        return "Person[name=" + name + ", heightMm=" + heightMm + "]";
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(!(obj instanceof Person)) {
+            return false;
+        }
+        Person that = (Person) obj;
+        return (this.name == that.name) && (this.heightMm == that.heightMm);
+    }
+}
