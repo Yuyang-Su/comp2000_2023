a2fc245ee4aef37c09ce2c0bfa43bc08d897dcc6
Yuyang-Su <yuyang.su1@students.mq.edu.au>
2023-07-31 12:51:58 +1000

    update Cell.java to draw individually

--- src/Cell.java
+++ src/Cell.java
@@ -1,6 +1,21 @@
 import java.awt.*;
+import javax.swing.*;
 
-public class Cell {
+public class Cell extends JPanel{
     int x;
     int y;
+
+    public Cell(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    //draws a single cell at the given x and y coordinates
+    
+    public void paint(Graphics g) {
+        g.setColor(Color.WHITE);
+        g.fillRect(x, y, 35, 35);
+        g.setColor(Color.BLACK);
+        g.drawRect(x, y, 35, 35);
+    }
 }
--- src/Grid.java
+++ src/Grid.java
@@ -9,10 +9,8 @@ class Canvas extends JPanel {
     public void paint(Graphics g) {
         for (int i = 10; i < 710; i += 35) {
             for (int j = 10; j < 710; j += 35) {
-                g.setColor(Color.WHITE);
-                g.fillRect(i, j, 35, 35);
-                g.setColor(Color.BLACK);
-                g.drawRect(i, j, 35, 35);
+                Cell cell = new Cell(i, j);
+                cell.paint(g);
             }
         }
     }
